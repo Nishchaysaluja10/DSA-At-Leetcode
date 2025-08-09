c533b58c5abc2082e8b0dbd346cc238ec0a745d2
NishchaySaluja <nishchaysaluja13@gmail.com>
2025-08-09 12:06:02 +0530

    Time: 57 ms (7.61%) | Memory: 18.9 MB (48.01%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,14 +4,12 @@ class Solution:
         j=1
         while j<len(nums):
             if nums[i]==nums[j]:
-                while nums.count(nums[i])!=1:
-                    nums.remove(nums[i])
-                i+=1
-                j=i+1
+                nums.pop(i)
             else:
                 i+=1
-                j=i+1
+                j+=1
             
         
                 
+            
             
