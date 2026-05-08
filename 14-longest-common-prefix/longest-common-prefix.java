74248a0cb7fd910b7db7f31cf960bbf3761ab27c
NishchaySaluja <nishchaysaluja13@gmail.com>
2026-05-08 23:58:01 +0530

    Time: 4 ms (13.27%) | Memory: 43.8 MB (5.36%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,9 +1,5 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // Arrays.sort(strs);
-        // System.out.println(strs[0]);
-        // List<List<String>> com = new ArrayList<>();
-        // HashMap<Character,Integer> ans = new HashMap<>();
         if (strs.length==0)return "";
         else if(strs.length==1)return strs[0];
         else if (strs[0].isEmpty()){
@@ -16,23 +12,6 @@ class Solution {
         boolean check = true ;
         while(check == true){  
             for(int i=0;i<strs.length;i++){
-                // if(!ans.containsKey(strs[i].charAt(check))){
-                //     Character ind = strs[i].charAt(check);
-                //     ans.put(ind,1);
-                // }
-                // else{
-                //     Character ind2 = strs[i].charAt(check);
-                //     ans.put(ind2,ans.get(strs[i].charAt(check))++);
-                // }
-                // System.out.println(strs[i].charAt(0));
-                // while(strs[i].charAt(check)==strs[i+1].charAt(check)){
-                //     curr = curr+strs[i].charAt(check);
-                //     check+=1;
-                // }
-                // check =0 ;
-                // if(curr.length()>answer.length()){
-                //     answer=curr;
-                // }
                 if( strs[i].length()==pos || strs[i].charAt(pos) !=  curr  ){
                 check = false;
                 break ; }
@@ -44,8 +23,6 @@ class Solution {
                 curr=strs[0].charAt(pos);
             }
         }
-        // System.out.println(curr);
-        // if(strs.length==1)return strs[0];
         return answer;
         }
     }
